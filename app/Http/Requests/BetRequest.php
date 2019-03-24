5564cc02cfce0058a7d84a90e55edfa57209cad0
ob2019 <ob.jn.2019@gmail.com>
2019-03-24 11:52:57 +0200

    * customizing output in case of validation errors

--- app/Http/Requests/BetRequest.php
+++ app/Http/Requests/BetRequest.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Support\Arr;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Symfony\Component\HttpFoundation\Response;
 
 class BetRequest extends FormRequest
 {
@@ -17,23 +20,11 @@ class BetRequest extends FormRequest
     }
 
     /**
-     * Get the validation rules that apply to the request.
+     * Custom error messages for BetRequest validations
      *
      * @return array
      */
-    public function rules()
-    {
-        return [
-            'player_id' => 'required|integer|min:1',
-            'stake_amount' => 'required|numeric|min:0.3|max:10000',
-            'selections' => 'required|array|min:1|max:20',
-
-            'selections.*.id' => 'required|distinct|integer|min:1',
-            'selections.*.odds' => 'required|numeric|min:1|max:10000',
-        ];
-    }
-
-    public function messages()
+    public function messages(): array
     {
         return [
             'player_id.required' => 'Betslip structure mismatch',
@@ -53,6 +44,72 @@ class BetRequest extends FormRequest
             'selections.*.odds.min' => 'Minimum odds are :min',
             'selections.*.odds.max' => 'Maximum odds are :max',
         ];
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'player_id' => 'required|integer|min:1',
+            'stake_amount' => 'required|numeric|min:0.3|max:10000',
+            'selections' => 'required|array|min:1|max:20',
+
+            'selections.*.id' => 'required|distinct|integer|min:1',
+            'selections.*.odds' => 'required|numeric|min:1|max:10000',
+        ];
+    }
+
+
+    /**
+     * Does some extra actions after validation is done
+     *
+     * @param Validator $validator
+     */
+    public function withValidator(Validator $validator): void
+    {
+        $validator->after(function (Validator $validator) {
+            //let's pick all errors
+            $errors = $validator->errors()->getMessages();
+
+            //if there are no errors - no more validation actions needed
+            if (empty($errors)) {
+                return;
+            }
+
+            //in case if there were some errors - let's change default validation error output to our custom one,
+            //described in api specification
+            $request = request()->all();
+
+            $globalErrors = [];
+            foreach ($errors as $k => $v) {
+                if (strpos($k, "selections.") === false) {
+                    //if this is global error
+                    $globalErrors[] = [
+                        'code' => 1,
+                        'message' => $v[0],
+                    ];
+                } else {
+                    //if this is selection error
+                    $key = substr($k,0,strrpos($k,'.'));
+
+                    Arr::set($request, $key . ".errors", [
+                        'code' => 2,
+                        'message' => $v['0']
+                    ]);
+                }
+            }
+
+            if (!empty($globalErrors)) {
+                $request['errors'] = $globalErrors;
+            }
+
+            response()->json($request, Response::HTTP_BAD_REQUEST)->send();
+            exit;
+        });
     }
 }
 
